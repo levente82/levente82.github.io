be293a924d09968be39c542caadcfd76696aad2e
levente82 <balazs_levente@freemail.hu>
2020-04-17 15:57:30 +0200

    Created the web application

---
+++ scripts.js
@@ -0,0 +1,150 @@
+let data = {
+    photo: ['./images/alexander-popov.jpg', './images/bridge.jpg', './images/harley-davidson.jpg', './images/james-mccullough.jpg', './images/jason-zhao.jpg', './images/manki-kim.jpg', './images/red-zeppelin.jpg', './images/vitor-pinto.jpg'],
+    title: ['Stores in Russia', 'Bridge near the sea', 'Ride on an Harley Davidson motor', 'Rocks', 'Junctions', 'Indoor plants', 'Lighthouse', 'Toyota'],
+    description: ['Local stores that are open at night too.', 'Bridge that connect Us.',
+    'Enjoy motorcycling....', 'Rocks everywhere...', 'From here you can go anywhere.',
+    'Indooor plants makes inside more like in the nature', 
+    'Lighthouse which shows the right way to the sailors and ships.',   
+    'A very attractive sport car.'
+]
+};
+
+let img = document.getElementById('img-container');
+let title = document.getElementById('image_title');
+let description = document.getElementById('image_description');
+let currentPhoto = 0;
+let up = document.getElementById('forward-arrow');
+let down = document.getElementById('back-arrow');
+let thumbnail_container = document.getElementById('thumbnails');
+let counter = 0;
+
+data.photo.forEach((i) => {
+    let newdiv = document.createElement('div');
+    newdiv.setAttribute('class', 'div'+counter+'');
+    newdiv.style.cssText = ('position: relative; text align: center; width: 80px; height: 80px, display: inline-block');
+    let thumbnail = new Image;
+    thumbnail.style.cssText = 'object-fit: cover; width: 70px; height: 70px; box-shadow: grey 0px 0px 3px 2px; padding: 3px; border-radius: 1px; position: relative';
+    thumbnail.setAttribute('id', counter);
+    //thumbnail.setAttribute('title', data.title[counter]);
+    let thumb_title = document.createElement('span');
+    thumb_title.setAttribute('id', 'thumb-title'+counter+'');
+    //thumb_title.style.cssText = 'position: relative; top: 0px; width: auto; text-align: center; background: black; padding: 10px, margin-bottom: 10px; color: white; visibility: hidden';
+    thumb_title.innerText = data.title[counter];
+    thumbnail.src = i;
+    counter++;
+    newdiv.appendChild(thumbnail);
+    newdiv.appendChild(thumb_title);
+    thumbnail_container.appendChild(newdiv);
+})
+
+let thumb0 = document.getElementById('0');
+let thumb1 = document.getElementById('1');
+let thumb2 = document.getElementById('2');
+let thumb3 = document.getElementById('3');
+let thumb4 = document.getElementById('4');
+let thumb5 = document.getElementById('5');
+let thumb6 = document.getElementById('6');
+let thumb7 = document.getElementById('7');
+
+function printImage(){
+    img.style.backgroundImage = 'url('+data.photo[currentPhoto]+')';
+    title.textContent = data.title[currentPhoto];
+    description.textContent = data.description[currentPhoto];
+    if (currentPhoto === 0) {
+        thumb0.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb0.style.background = 'inherit';
+    }
+    if (currentPhoto === 1) {
+        thumb1.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb1.style.background = 'inherit';
+    }
+    if (currentPhoto === 2) {
+        thumb2.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb2.style.background = 'inherit';
+    }
+    if (currentPhoto === 3) {
+        thumb3.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb3.style.background = 'inherit';
+    }
+    if (currentPhoto === 4) {
+        thumb4.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb4.style.background = 'inherit';
+    }
+    if (currentPhoto === 5) {
+        thumb5.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb5.style.background = 'inherit';
+    }
+    if (currentPhoto === 6) {
+        thumb6.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb6.style.background = 'inherit';
+    }
+    if (currentPhoto === 7) {
+        thumb7.style.background = 'rgb(65 179 247)';
+    } else {
+        thumb7.style.background = 'inherit';
+    }
+}
+
+printImage();
+
+//
+up.onclick = function() {
+    if (currentPhoto === 7) {
+        currentPhoto = 0;
+        printImage();
+    } else {
+        currentPhoto++;
+        printImage();
+    }
+}
+
+down.onclick = function() {
+    if (currentPhoto === 0) {
+        currentPhoto = 7;
+        printImage();
+    } else {
+        currentPhoto--;
+        printImage();
+    }
+}
+
+//
+thumb0.onclick = function() {
+    currentPhoto = 0;
+    printImage();
+}
+thumb1.onclick = function() {
+    currentPhoto = 1;
+    printImage();
+}
+thumb2.onclick = function() {
+    currentPhoto = 2;
+    printImage();
+}
+thumb3.onclick = function() {
+    currentPhoto = 3;
+    printImage();
+}
+thumb4.onclick = function() {
+    currentPhoto = 4;
+    printImage();
+}
+thumb5.onclick = function() {
+    currentPhoto = 5;
+    printImage();
+}
+thumb6.onclick = function() {
+    currentPhoto = 6;
+    printImage();
+}
+thumb7.onclick = function() {
+    currentPhoto = 7;
+    printImage();
+}
